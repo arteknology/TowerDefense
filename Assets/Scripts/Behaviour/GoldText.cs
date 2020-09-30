cd3120b562c6b30fab65c8be21fe870ebb1c6f24
arteknology <hauton.arthur@gmail.com>
2020-09-30 13:05:44 +0200

    Fix Bug Gold

--- Assets/Scripts/Behaviour/GoldText.cs
+++ Assets/Scripts/Behaviour/GoldText.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class GoldText : MonoBehaviour
 {
-    private TextAlignment text;
+    private Text text;
     private PlayerBehaviour _player;
     void Start()
     {
         text = GetComponent<Text>();
-        _player = GameObject.FindWithTag("Player");
+        _player = GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>();
     }
 
     private void Update()
     {
-        text.text = " Gold: " + PlayerBehaviour.gold;
+        text.text = " Gold: " + _player.gold;
     }
 
 
